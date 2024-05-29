f6201102cf06c92fb25ec02ae21a848bfc85d4f5
jeevanandham010 <jeevakathirvel3699@gmail.com>
2024-05-29 15:38:23 +0530

    first commit

---
+++ src/main/java/com/example/demo/TransactionManagementSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionManagementSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionManagementSbApplication.class, args);
+	}
+
+}
